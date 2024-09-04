8a37a2b9023ffcc8e2d13abd81a856bd65baad70
SantiBalvin <santiagoalvarezbalvin12312@gmail.com>
2024-09-04 13:04:25 -0500

    Navbar actualizado con nuevos nombres de las carpetas.

--- config/site.ts
+++ config/site.ts
@@ -1,121 +1,102 @@
 export type SiteConfig = typeof siteConfig;
-
 export const siteConfig = {
   name: "Alexandra Torres",
   description: "",
   navItems: [
-    /*{
-      categoryLabel: "General",
-      items: [
-        { label: "Inicio", href: "/" }
-      ]
-    },*/
-    
     {
       categoryLabel: "Administración",
       items: [
-        { label: "Roles", href: "/admin/roles", permiso:"Gestionar Roles" },
-        { label: "Usuarios", href: "/admin/usuarios", permiso:"Gestionar Usuarios" },
-        { label: "Clientes", href: "/admin/clientes", permiso:"Gestionar Clientes" }
+        { label: "Roles", href: "/admin/roles", permiso: "Gestionar Roles" },
+        { label: "Usuarios", href: "/admin/usuarios", permiso: "Gestionar Usuarios" },
+        { label: "Clientes", href: "/admin/clientes", permiso: "Gestionar Clientes" }
       ]
     },
     {
       categoryLabel: "Ventas",
       items: [
-        { label: "Ventas", href: "/admin/ventas", permiso:"Gestionar Ventas" },
-        // { label: "Detalles Productos", href: "/admin/ventas/dp" },
-        //{ label: "Detalles Servicios", href: "/admin/ventas/ds" }
+        { label: "Ventas", href: "/admin/ventas", permiso: "Gestionar Ventas" }
       ]
     },
     {
       categoryLabel: "Compras",
       items: [
-        { label: "Proveedores", href: "/admin/compras/proveedores", permiso:"Gestionar Proveedores" },
-        { label: "Salida Insumos", href: "/admin/compras/SalidaInusmos", permiso:"Gestionar Insumos" },
-        { label: "Categoría Producto", href: "/admin/compras/categoriaProducto", permiso:"Gestionar Categoria de Productos" },
-        { label: "Producto", href: "/admin/compras/producto", permiso:"Gestionar Producto" },
-        { label: "Compra", href: "/admin/compras/compra", permiso:"Gestionar Compras" }
+        { label: "Proveedores", href: "/admin/compras/proveedores", permiso: "Gestionar Proveedores" },
+        { label: "Salida Insumos", href: "/admin/compras/salida_insumos", permiso: "Gestionar Insumos" },
+        { label: "Categoría Producto", href: "/admin/compras/categoria_producto", permiso: "Gestionar C. Productos" },
+        { label: "Producto", href: "/admin/compras/producto", permiso: "Gestionar Producto" },
+        { label: "Compra", href: "/admin/compras", permiso: "Gestionar Compras" }
       ]
     },
     {
       categoryLabel: "Agendamiento",
       items: [
-        { label: "Citas", href: "/admin/Agendamiento/citas", permiso:"Gestionar Agendamiento" },
-        { label: "Colaboradores", href: "/admin/Agendamiento/colaboradores", permiso:"Gestionar Colaboradores" },
-        { label: "Horarios", href: "/admin/Agendamiento/horario", permiso:"Gestionar Horario" },
-        { label: "Motivos", href: "/admin/Agendamiento/motivo", permiso:"Gestionar Agendamiento" },
-        { label: "Horarios", href: "/colaborador", permiso:"Gestionar C Horario" },
+        { label: "Citas", href: "/admin/agendamiento/citas", permiso: "Gestionar Agendamiento" },
+        { label: "Colaboradores", href: "/admin/agendamiento/colaboradores", permiso: "Gestionar Colaboradores" },
+        { label: "Horarios", href: "/admin/agendamiento/horario", permiso: "Gestionar Horario" },
+        { label: "Motivos", href: "/admin/agendamiento/motivo", permiso: "Gestionar Agendamiento" },
+        { label: "Colaborador", href: "/colaborador", permiso: "Gestionar C Horario" }
       ]
     },
     {
       categoryLabel: "Servicios",
       items: [
-        { label: "Servicios", href: "/admin/servicios", permiso:"Gestionar Servicios" },
-        { label: "Paquetes", href: "/admin/paquetes", permiso:"Gestionar Paquetes" }
+        { label: "Servicios", href: "/admin/servicios", permiso: "Gestionar Servicios" },
+        { label: "Paquetes", href: "/admin/paquetes", permiso: "Gestionar Paquetes" }
       ]
     },
     {
       categoryLabel: "Clientes",
       items: [
-        { label: "Citas", href: "/cliente", permiso:"Gestionar Cita" },
+        { label: "Citas", href: "/cliente", permiso: "Gestionar Cita" }
       ]
     }
-    
   ],
   navMenuItems: [
-    /*{
-      categoryLabel: "General",
-      items: [
-        { label: "Inicio", href: "/" }
-      ]
-    },*/
     {
       categoryLabel: "Administración",
       items: [
-        { label: "Roles", href: "/admin/roles", permiso:"Gestionar Roles" },
-        { label: "Usuarios", href: "/admin/usuarios", permiso:"Gestionar Usuarios" },
-        { label: "Clientes", href: "/admin/clientes", permiso:"Gestionar Clientes" }
+        { label: "Roles", href: "/admin/roles", permiso: "Gestionar Roles" },
+        { label: "Usuarios", href: "/admin/usuarios", permiso: "Gestionar Usuarios" },
+        { label: "Clientes", href: "/admin/clientes", permiso: "Gestionar Clientes" }
       ]
     },
     {
       categoryLabel: "Ventas",
       items: [
-        { label: "Ventas", href: "/admin/ventas", permiso:"Gestionar Ventas" },
-        //{ label: "Detalles Productos", href: "/admin/ventas/dp" },
-        //{ label: "Detalles Servicios", href: "/admin/ventas/ds" }
+        { label: "Ventas", href: "/admin/ventas", permiso: "Gestionar Ventas" }
       ]
     },
     {
       categoryLabel: "Compras",
       items: [
-        { label: "Proveedores", href: "/admin/compras/proveedores", permiso:"Gestionar Proveedores" },
-        { label: "Salida Insumos", href: "/admin/compras/SalidaInusmos", permiso:"Gestionar Insumos" },
-        { label: "Categoría Producto", href: "/admin/compras/categoriaProducto", permiso:"Gestionar Categoria de Productos" },
-        { label: "Producto", href: "/admin/compras/producto", permiso:"Gestionar Producto" },
-        { label: "Compra", href: "/admin/compras/compra", permiso:"Gestionar Compras" }
+        { label: "Proveedores", href: "/admin/compras/proveedores", permiso: "Gestionar Proveedores" },
+        { label: "Salida Insumos", href: "/admin/compras/salida_insumos", permiso: "Gestionar Insumos" },
+        { label: "Categoría Producto", href: "/admin/compras/categoria_insumos", permiso: "Gestionar C. Productos" },
+        { label: "Producto", href: "/admin/compras/producto", permiso: "Gestionar Producto" },
+        { label: "Compra", href: "/admin/compras/compra", permiso: "Gestionar Compras" }
       ]
     },
     {
       categoryLabel: "Agendamiento",
       items: [
-        { label: "Citas", href: "/admin/Agendamiento/citas", permiso:"Gestionar Agendamiento" },
-        { label: "Colaboradores", href: "/admin/Agendamiento/colaboradores", permiso:"Gestionar Colaboradores" },
-        { label: "Horarios", href: "/admin/Agendamiento/horario", permiso:"Gestionar Horario" },
-        { label: "Motivos", href: "/admin/Agendamiento/motivo", permiso:"Gestionar Agendamiento" }
+        { label: "Citas", href: "/admin/Agendamiento/citas", permiso: "Gestionar Agendamiento" },
+        { label: "Colaboradores", href: "/admin/Agendamiento/colaboradores", permiso: "Gestionar Colaboradores" },
+        { label: "Horarios", href: "/admin/Agendamiento/horario", permiso: "Gestionar Horario" },
+        { label: "Motivos", href: "/admin/Agendamiento/motivo", permiso: "Gestionar Agendamiento" }
       ]
     },
     {
       categoryLabel: "Servicios",
       items: [
-        { label: "Servicios", href: "/admin/servicios", permiso:"Gestionar Sevicios" },
-        { label: "Paquetes", href: "/admin/paquetes", permiso:"Gestionar Paquetes" }
+        { label: "Servicios", href: "/admin/servicios", permiso: "Gestionar Sevicios" },
+        { label: "Paquetes", href: "/admin/paquetes", permiso: "Gestionar Paquetes" }
       ]
     },
     {
       categoryLabel: "Cuenta",
       items: [
-        { label: "Crear Cuenta", href: "/acceso/registro", permiso:"noToken" },
-        { label: "Iniciar Sesión", href: "/acceso/iniciarsesion", permiso:"noToken" }
+        { label: "Crear Cuenta", href: "/acceso/registro", permiso: "noToken" },
+        { label: "Iniciar Sesión", href: "/acceso/iniciarsesion", permiso: "noToken" }
       ]
     }
   ],
